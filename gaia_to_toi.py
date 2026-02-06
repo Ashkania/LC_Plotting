e56cf798b836079a86335d6bab278fa6a0f11fe1
Ashkania <sxj190009@utdallas.edu>
2026-02-06 17:41:19 -0600

    Improve documentation

--- gaia_to_toi.py
+++ gaia_to_toi.py
@@ -2,15 +2,49 @@
 
 """
     Utilities for plotting individual lightcurves.
-    It goes over the LCs, extract the Gaia Ids, then run tap_vizier_query
-    for querying TIC for those ids.
-    Then, it queries TOI table in exoplanet archive and extract the objects
-    in the FOV of the LCs and check if any of those TOI have a TIC id in our
-    list. If so, it extracts the period.
-    Also, it writes two files:
-        1- tic_gaia.txt: tic id, gaia id (for later use cases)
-        2- toi_gaia_period.txt: toi id, gaia id, period
+    
+    Command line arguments:
+    --lc-path: The path containing .h5 lc files.
+    --tic-gaia-fname: The output file to write tic to gaia mappings.
+        --tic-gaia-file: if we already have it, specify the file path
+    --toi-gaia-period-fname: The output file to write toi, gaia, period mappings.
+    --lc-catalog: The input file containing the light curve catalog.
 
+    1. We have the LCs, want to extract their Gaia ids:
+        lcs_to_gaia_ids(lc_path) -> gaia_ids
+        It goes over the LCs, and extract their Gaia Ids from their names,
+        and return the unique set of them.
+    
+    (*). We need the FOV and mag range of the LCs to be used in step 3.*** 
+        find_fov_of_lcs(lc_catalog) -> fov_mag_range = {ra, dec, mag_{min, max}}
+        It goes over the LC catalog and find the range (min, max) of ra, dec, and mag.
+
+    3. To extract TICs from Gaia ids, we have three options:
+        
+        3.*. If we already have the tic_gaia mapping from a previous run,
+        we can just read the related file and use it.
+
+        query_vizier_n_times(gaia_ids, tic_gaia_fname) -> tic_gaia
+        3.**. For each Gaia id, query vizier to find its TIC id
+        We then write the tic_gaia mapping to a file for later use cases,
+        and also to avoid querying vizier again if we already have it.
+        (we may use this one for a small number of LCs, like < 1000)
+        
+        query_vizier_once(gaia_ids, tic_gaia_fname, fov_mag_range) -> tic_gaia
+        3.***. Query vizier once based on the FOV of the LCs (ra, dec, mag range)
+        that we got in (*) to find all TICs in that area.
+        Then, we check the result by going over each row and check if the Gaia id
+        in that row is in our list of Gaia ids from the LCs.
+        If so, we save the TIC id and Gaia id in a dictionary.
+        (we may use this one for a large number of LCs, like > 1000)
+
+    4. Extract TOI IDs and their periods for the TICs that we found. 
+    query_toi_in_fov(tic_gaia, cmdline_args.toi_gaia_period_fname, fov_mag_range) -> None
+    Finally, it queries TOI table in exoplanet archive in the range in the FOV of the LCs
+    and check if any of those TOI have a TIC id in our list. If so, it extracts the period.
+    Also, it writes: toi_gaia_period.txt
+    -------------------------------------------------
+        
     Check this link for all tables that you may need:
     https://tapvizier.cds.unistra.fr/adql/
     more info in discord:
