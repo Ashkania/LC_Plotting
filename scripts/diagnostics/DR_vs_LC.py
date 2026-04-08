a97d5547d175d6f64693b1db06be4594bc688c71
Ashkania <sxj190009@utdallas.edu>
2026-04-07 20:18:58 -0500

    add fitted magnitudes read in DR/LC diagnostic

--- scripts/diagnostics/DR_vs_LC.py
+++ scripts/diagnostics/DR_vs_LC.py
@@ -3,6 +3,10 @@
 # TODO: Given a Gaia id, go over all the DR files, extract the x,y
 # of that source, and compare the number of points we find with 
 # what we have in the LC
+# ex of cmdline usage:
+# for tel in *; do for cam in $tel/*; do echo "$cam";
+# /home/sxj190009/LC_Plotting/scripts/diagnostics/DR_vs_LC.py
+# --gaia-id 3334901543952614528 --dr-path "$cam"; done ; done
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,18 +35,18 @@ def load_data(dr, gaia_id):
     with h5py.File(dr, 'r') as f:
         try:
             gaia_ids = f['/ProjectedSources/Version000/source_id'][:]
-            # print(gaia_ids)
-            # print(gaia_id)
             gaia_index = np.where(gaia_ids == int(gaia_id))[0]
-            # print(gaia_index)
         
             x = f['/ProjectedSources/Version000/x'][:][gaia_index]
             y = f['/ProjectedSources/Version000/y'][:][gaia_index]
+
+            fitted_mag = f['/AperturePhotometry/Version000/Aperture000/FittedMagnitudes/Version000/Iteration001'][:][gaia_index]
+
         except:
             # print(f"Gaia ID {gaia_id} not found in DR file")
-            return None, None
-    
-    return x, y
+            return None, None, None
+        
+    return x, y, fitted_mag
 
 
 
@@ -51,15 +55,19 @@ def main():
     args = parse_args()
     X = []
     Y = []
+    Fitted_Mag = []
     for dr in glob.glob(args.dr_path + "/*.h5"):
         # print(f"Processing DR file: {dr}")
-        x, y = load_data(dr, args.gaia_id)
-        if x is not None and y is not None and x.size > 0 and y.size > 0:
+        x, y, fitted_mag = load_data(dr, args.gaia_id)
+        if x is not None and x.size > 0:
             X.append(x.item())
             Y.append(y.item())
+            if fitted_mag is not None and fitted_mag.size > 0 and fitted_mag > -1000:
+                Fitted_Mag.append(fitted_mag.item())
     if X and Y:
         print(f'X) min: {min(X)}, max: {max(X)}, len: {len(X)}')
         print(f'Y) min: {min(Y)}, max: {max(Y)}, len: {len(Y)}')
+        print(f'FitMag) min: {min(Fitted_Mag)}, max: {max(Fitted_Mag)}, len: {len(Fitted_Mag)}')
     else:
         print("No valid data found for the given Gaia ID across the DR files.")
 
